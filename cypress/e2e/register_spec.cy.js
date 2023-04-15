bfea84721d5bad5ce4ae10d54104117554578f38
Tom Doder <tdoder89@gmail.com>
2023-04-15 13:09:51 -0500

    add changes to register page cypress testing

--- cypress/e2e/register_spec.cy.js
+++ cypress/e2e/register_spec.cy.js
@@ -1,16 +1,16 @@
-describe('Login Page', () => {
+describe('Register Page', () => {
     beforeEach(() => {
     cy.intercept('GET', 'https://api.nytimes.com/svc/books/v3/lists/full-overview.json?api-key=JL2wo9KGenD4ya0vsuAP1IGxduivvAwS',{fixture: "lists.json"})
     cy.visit('http://localhost:3000/register')
 })
-it('Should a 6 letter password', () => {
+it('Should a 6 character password', () => {
     cy.get('#first-name').type('testing')
     cy.get('#email').type('test@testing.com')
     cy.get('#pass').type('pass')
     cy.get('.submit').click()
     cy.get('.error-code').should('contain', 'Password must be at least 6 characters')
  })
- it('Should require email', () => {
+ it('Should require a valid email', () => {
     cy.get('#first-name').type('testing')
     cy.get('#email').type('test')
     cy.get('#pass').type('password')
