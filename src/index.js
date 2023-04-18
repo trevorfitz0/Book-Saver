ef3bfeead545d8884ba303ceee52d2327b3d89dc
trevorfitz0 <trevorfitz0@gmail.com>
2023-04-17 18:31:30 -0600

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App/App.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
